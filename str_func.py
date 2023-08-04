1d289be6415a3948b6fd27204b75677ef61df454
Denis Prusakov <Denikpr@gmail.com>
2023-08-04 16:04:13 +0300

    add def title_text

--- str_func.py
+++ str_func.py
@@ -1,6 +1,7 @@
 def upp_text(input_text):
-    """
-    переводит текст в заглавные буквы
-    """
     upp_text = input_text.upper()
     return upp_text()
+
+def title_text(input_text):
+    title_text = input_text.title()
+    return title_text()
